4595e4c3572c8c6d04bb6e369ac12ca993e059e8
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-08-16 18:17:29 +0300

    Add user's avatar generator.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 from app import db, login
 
@@ -21,6 +22,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        gravatar_url = 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'
+        return gravatar_url.format(digest, size)
 
 
 class Post(db.Model):
